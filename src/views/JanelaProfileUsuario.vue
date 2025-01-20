bdc586f51ea319ef93f82818c4bfbc6b2b3c1bef
Ana.Tirolli <acampos-tirolli@myseneca.ca>
2025-01-20 06:04:45 -0500

    pessoas - sumiu??

--- src/views/JanelaProfileUsuario.vue
+++ src/views/JanelaProfileUsuario.vue
@@ -29,6 +29,7 @@
 
     <!-- Coluna direita: Componentes "Pessoas que talvez conheça" e "Trends" -->
     <div class="main-right col-span-1 space-y-4">
+      <PessoasQueTalvezConheca />
       <Trends />
     </div>
   </div>
@@ -36,6 +37,7 @@
 
 <script>
 import axios from "axios";
+import PessoasQueTalvezConheca from "../components/PessoasQueTalvezConheca.vue";
 import Trends from "../components/Trends.vue";
 import ProfileUsuario from "../components/ProfileUsuario.vue";
 import Feed from "../components/Feed.vue";
@@ -44,6 +46,7 @@ import CaixaPost from "../components/CaixaPost.vue";
 export default {
   name: "JanelaProfile",
   components: {
+    PessoasQueTalvezConheca,
     Trends,
     ProfileUsuario,
     Feed,
