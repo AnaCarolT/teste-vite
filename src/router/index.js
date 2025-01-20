d2d8fb1006f0190d41114b729d2ba004eecdcd0e
Ana.Tirolli <acampos-tirolli@myseneca.ca>
2025-01-20 05:18:33 -0500

    Index

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import MenuPrincipal from "@/views/MenuPrincipal.vue";
-import Usuarios from "@/views/Usuarios.vue"; // Este será o componente usado para exibir a lista de usuários
-import Chat from "@/views/Chat.vue";
-import JanelaProfileUsuario from "@/views/JanelaProfileUsuario.vue";
-import DetalhesPosts from "@/views/DetalhesPosts.vue"; // Importar componente
-import Notificacoes from "@/views/Notificacoes.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'; // Use hash history
+import MenuPrincipal from '@/views/MenuPrincipal.vue';
+import Usuarios from '@/views/Usuarios.vue';
+import Chat from '@/views/Chat.vue';
+import JanelaProfileUsuario from '@/views/JanelaProfileUsuario.vue';
+import DetalhesPosts from '@/views/DetalhesPosts.vue';
+import Notificacoes from '@/views/Notificacoes.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL), // Hash-based routing
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: MenuPrincipal,
     },
     {
-      path: "/users", // A rota para a lista de usuários
-      name: "users",
-      component: Usuarios, // Usando Usuarios.vue aqui
+      path: '/users',
+      name: 'users',
+      component: Usuarios,
     },
     {
-      path: "/profile/:id",
-      name: "detalhes-profile",
+      path: '/profile/:id',
+      name: 'detalhes-profile',
       component: JanelaProfileUsuario,
     },
     {
-      path: "/post/:postId",
-      name: "detalhes-posts",
-      component: DetalhesPosts, // Rota para detalhes do post
+      path: '/post/:postId',
+      name: 'detalhes-posts',
+      component: DetalhesPosts,
     },
     {
-      path: "/chat",
-      name: "chat",
+      path: '/chat',
+      name: 'chat',
       component: Chat,
     },
     {
-      path: "/notificacoes",
-      name: "notificacoes",
+      path: '/notificacoes',
+      name: 'notificacoes',
       component: Notificacoes,
     },
   ],
